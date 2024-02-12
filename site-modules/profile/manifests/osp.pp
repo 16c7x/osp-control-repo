2d2deda4d950c290b728f226f64910de26866223
Andrew Jones <andrewj100@gmail.com>
2024-02-12 17:23:53 +0000

    Update osp.pp

--- site-modules/profile/manifests/osp.pp
+++ site-modules/profile/manifests/osp.pp
@@ -1,5 +1,9 @@
 # The base profile should include component modules that will be on all nodes
 class profile::osp {
   notify {'osp server' : }
-  include puppetdb::master::config
+  # Configure puppetdb and its underlying database
+  class { 'puppetdb': }
+  
+  # Configure the Puppet master to use puppetdb
+  class { 'puppetdb::master::config': }
 }
