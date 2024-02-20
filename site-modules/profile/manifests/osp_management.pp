70be5d208d2409351edfe5c875f44e570dd74058
Andrew Jones <andrewj100@gmail.com>
2024-02-20 13:20:41 +0000

    Update and rename osp.pp to osp_management.pp

--- site-modules/profile/manifests/osp_management.pp
+++ site-modules/profile/manifests/osp_management.pp
@@ -1,9 +1,14 @@
 # The base profile should include component modules that will be on all nodes
-class profile::osp {
+class profile::osp_management {
   notify {'osp server' : }
   # Configure puppetdb and its underlying database
   class { 'puppetdb': }
   
   # Configure the Puppet master to use puppetdb
   class { 'puppetdb::master::config': }
+
+  package { 'r10k' :
+    ensure   => installed,
+    provider => puppet_gem,
+  }
 }
